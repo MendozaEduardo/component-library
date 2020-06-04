a80133f7af0d47badc2b5915488791eb7ef28b8e
Eduardo Mendoza <mendozae96@gmail.com>
2020-06-04 16:32:41 -0500

    First commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import PrimaryButton, {SecondaryButton, TertiaryButton} from "./components/Buttons";
+
+const App = () => (
+    <div>
+        <PrimaryButton>Hello World!</PrimaryButton>
+        <SecondaryButton>Hello World!</SecondaryButton>
+        <TertiaryButton>Hello World!</TertiaryButton>
+    </div>
+)
+ReactDOM.render(<App />, document.getElementById("root"));
